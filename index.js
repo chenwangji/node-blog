45d376a6dd1f8475f3fe0a6e1666679e99a2d971
chenwangji <chenwangji@hebao.im>
2018-06-11 11:34:16 +0800

    feat: 增加日志

--- index.js
+++ index.js
@@ -6,6 +6,8 @@ const flash = require('connect-flash')
 const config = require('config-lite')(__dirname)
 const routes = require('./routes')
 const pkg = require('./package')
+const winston = require('winston')
+const expressWinston = require('express-winston')
 
 const app = express()
 
@@ -51,9 +53,32 @@ app.use(function (req, res, next) {
   res.locals.error = req.flash('error').toString()
   next()
 })
-
+// 正常请求的日志
+app.use(expressWinston.logger({
+  transports: [
+    new (winston.transports.Console)({
+      json: true,
+      conlorize: true
+    }),
+    new winston.transports.File({
+      filename: 'logs/success'
+    })
+  ]
+}))
 // 路由
 routes(app)
+// 错误请求日志
+app.use(expressWinston.errorLogger({
+  transports: [
+    new winston.transports.Console({
+      json: true,
+      conlorize: true
+    }),
+    new winston.transports.File({
+      filename: 'logs/error.log'
+    })
+  ]
+}))
 
 // 错误处理
 app.use((err, req, res, next) => {
